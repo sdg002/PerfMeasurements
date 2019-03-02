de5db48c78bcc865087e6de8372a4fd235bd3623
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2019-03-02 22:53:46 +0000

    (*)Added StringConcat

---
+++ StringConcat.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PerfMeasurements
+{
+    /// <summary>
+    /// 
+    /// 
+    /// The performance of String.Concat and StringBuilder is identical!!
+    /// 
+    /// 
+    /// 
+    /// </summary>
+    [TestClass]
+    public class StringConcat
+    {
+        const int MAXITERATIONS = 10000000;
+        const int MAXCOUNTOF_INTEGERS = 20;
+        [TestMethod]
+        public void ConcatUsingStringConcatMethod()
+        {
+            int[] arrTest = Util.CreateRandomArrayOfInt(MAXCOUNTOF_INTEGERS);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < MAXITERATIONS; i++)
+            {
+                string output = string.Concat(arrTest);
+            }
+            sw.Stop();
+            double elapsedTotal = sw.ElapsedMilliseconds;
+            double elapsedAverage = elapsedTotal / MAXITERATIONS;
+            Trace.WriteLine("String.Concat method");
+            Trace.WriteLine($"Elapsed average={elapsedAverage} ms   Elapsed Total={elapsedTotal} ms");
+            Util.ThrowExceptionIfNot64BitAndNotRelease();
+        }
+        [TestMethod]
+        public void ConcatUsingStringBuilder()
+        {
+            int[] arrTest = Util.CreateRandomArrayOfInt(MAXCOUNTOF_INTEGERS);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < MAXITERATIONS; i++)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach(int r in arrTest)
+                {
+                    sb.Append(r);
+                }
+                string output = sb.ToString();
+            }
+            sw.Stop();
+            double elapsedTotal = sw.ElapsedMilliseconds;
+            double elapsedAverage = elapsedTotal / MAXITERATIONS;
+            Trace.WriteLine("StringBuilder");
+            Trace.WriteLine($"Elapsed average={elapsedAverage} ms   Elapsed Total={elapsedTotal} ms");
+            Util.ThrowExceptionIfNot64BitAndNotRelease();
+        }
+        [TestMethod]
+        public void ConcatUsingStringSimpleAddition()
+        {
+            int[] arrTest = Util.CreateRandomArrayOfInt(MAXCOUNTOF_INTEGERS);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < MAXITERATIONS; i++)
+            {
+                string sSimpleString = "";
+                foreach(int r in arrTest)
+                {
+                    sSimpleString += r.ToString();
+
+                }
+            }
+            sw.Stop();
+            double elapsedTotal = sw.ElapsedMilliseconds;
+            double elapsedAverage = elapsedTotal / MAXITERATIONS;
+            Trace.WriteLine("Simple addition of strings");
+            Trace.WriteLine($"Elapsed average={elapsedAverage} ms   Elapsed Total={elapsedTotal} ms");
+            Util.ThrowExceptionIfNot64BitAndNotRelease();
+        }
+    }
+}
--- Util.cs
+++ Util.cs
@@ -8,6 +8,7 @@ namespace PerfMeasurements
 {
     public class Util
     {
+        private static Random _rnd = new Random(DateTime.Now.Second);
         /// <summary>
         /// Throws an Exception if any of the following conditions are met:
         ///     1)Code is compiled in Debug mode 
@@ -24,6 +25,21 @@ namespace PerfMeasurements
             {
                 throw new Exception("This test will work in 64 bit only");
             }
+        }
+        /// <summary>
+        /// Creates an array of Int with the specified number of random numbers
+        /// </summary>
+        /// <param name="count">Total no. of random nos to generate</param>
+        /// <returns></returns>
+        internal static int[] CreateRandomArrayOfInt(int count)
+        {
+            List<int> results = new List<int>();
+            for(int index=0;index<count;index++)
+            {
+                int r = _rnd.Next();
+                results.Add(r);
+            }
+            return results.ToArray();
         }
     }
 }
